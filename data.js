66323c3076ed42387f977181408e37b20d0a57db
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-12-11 20:39:49 -0300

    Criados os produtos na API

--- data.js
+++ data.js
@@ -12,6 +12,71 @@ const data = {
       name: "Hambúrgueres",
       image:"/images/burgers.jpg"
     }
+  ],
+  products: [
+    {
+      category: "Bebidas",
+      name: "Coca-Cola",
+      calorie: 120,
+      price: 1,
+      image: "/images/t-mcdonalds-Coca-Cola-Classic-Small.jpg"
+    },
+    {
+      category: "Bebidas",
+      name: "Milkshake de Baunilha",
+      price: 1.5,
+      calorie: 360,
+      image: "/images/t-mcdonalds-Vanilla-McCafe-Shake-Medium.jpg",
+    },
+    {
+      category: "Bebidas",
+      name: "Chocolate Quente",
+      price: 2,
+      calorie: 170,
+      image: "/images/t-mcdonalds-McCafe-Hot-Chocolate-Medium.jpg",
+    },
+    {
+      category: "Acompanhamentos",
+      name: "McMuffin",
+      price: 1.9,
+      calorie: 90,
+      image: "/images/t-mcdonalds-Bacon-Egg-Cheese-Biscuit-Regular-Size-Biscuit.jpg",
+    },
+    {
+      category: "Acompanhamentos",
+      name: "Muffin",
+      price: 1.5,
+      calorie: 120,
+      image: "/images/t-blueberry-muffin.jpg",
+    },
+    {
+      category: "Acompanhamentos",
+      name: "Big Breakfast",
+      price: 3,
+      calorie: 430,
+      image: "/images/s-mcdonalds-Big-Breakfast-Regular-Size-Biscuit.jpg",
+    },
+    {
+      category: "Hambúrgueres",
+      name: "Big Mac",
+      price: 1.9,
+      calorie: 200,
+      image: "/images/t-mcdonalds-Big-Mac.jpg",
+    },
+    {
+      category: "Burgers",
+      name: "Hamburger",
+      price: 1.5,
+      calorie: 410,
+      image: "/images/t-mcdonalds-Hamburger.jpg",
+    },
+    {
+      category: "Hambúrgueres",
+      name: "McDouble",
+      price: 3,
+      calorie: 320,
+      image: "/images/t-mcdonalds-McDouble.jpg",
+    }
   ]
 };
 
--- server.js
+++ server.js
@@ -1,11 +1,62 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const data = require("./data");
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/*
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  });
+*/
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected!"))
+  .catch((error) => console.log(error.message));
+
+const ProductsSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  image: String,
+  price: Number,
+  calorie: Number,
+  category: String
+});
+
+const Products = mongoose.model("Products", ProductsSchema);
+
+app.get("/products", async (req, res) => {
+  const products = await Products.insertMany(data.products);
+
+  res.send({ products });
+});
+
+app.get("/products/:category", async (req, res) => {
+  const { category } = req.params;
+
+  const products = await Products.find(category ? { category } : {});
+
+  res.send(products);
+});
+
+app.post("products", async(req, res) => {
+  const newProduct = new Products(req.body);
+  
+  const savedProduct = await newProduct.save();
+
+  res.send(savedProduct);
+});
 
 app.get("/categories", (req, res) => {
   res.send(data.categories);
