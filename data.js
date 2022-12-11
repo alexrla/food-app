ac252ae352ef821a4095cdbf2b6a9ed0a98e1374
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-12-11 15:25:51 -0300

    Inicializada a API, listando as categorias

---
+++ data.js
@@ -0,0 +1,18 @@
+const data = {
+  categories: [
+    {
+      name: "Bebidas",
+      image: "./src/assets/imgs/beverages.jpg"
+    },
+    {
+      name: "Acompanhamentos",
+      image:"./src/assets/imgs/breakfast.jpg"
+    },
+    {
+      name: "Hambúrgueres",
+      image:"./src/assets/imgs/burgers.jpg"
+    }
+  ]
+};
+
+module.exports = data;
